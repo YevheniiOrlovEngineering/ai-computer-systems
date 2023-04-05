eeace7e6fb83ac6bc0d72596352c85c8ac41ab72
Yevhenii Orlov <orlovowork@gmail.com>
2023-04-05 03:15:37 +0300

    LAB-2 Added fictional training

---
+++ lab-2/train/train.py
@@ -0,0 +1,90 @@
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.losses import categorical_crossentropy
+from keras.optimizers import Adam
+from keras.regularizers import l2
+
+num_features = 64
+num_labels = 7
+batch_size = 64
+epochs = 100
+width, height = 48, 48
+
+x = np.load('./dataX.npy')
+y = np.load('./labels.npy')
+
+x -= np.mean(x, axis=0)
+x /= np.std(x, axis=0)
+
+
+# splitting into training, validation and testing data
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.1, random_state=41)
+
+#saving the test samples to be used later
+np.save('modXtest', X_test)
+np.save('modytest', y_test)
+
+# desinging the CNN
+model = Sequential()
+
+model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', input_shape=(width, height, 1), data_format='channels_last', kernel_regularizer=l2(0.01)))
+model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+
+model.add(Dense(2*2*2*num_features, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(2*2*num_features, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(2*num_features, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(num_labels, activation='softmax'))
+
+# Compliling the model with adam optimixer and categorical crossentropy loss
+model.compile(loss=categorical_crossentropy,
+              optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-7),
+              metrics=['accuracy'])
+
+# training the model
+model.fit(np.array(X_train), np.array(y_train),
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(np.array(X_valid), np.array(y_valid)),
+          shuffle=True)
+
+# saving the  model to be used later
+model_name = os.getenv("MODEL_NAME")
+
+model.save_weights(model_name)
+print("Saved model to disk")
